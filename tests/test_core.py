7af4e466f991dab0bebbffd4afa213446f3c7b77
Charles Turner <charles.turner@anu.edu.au>
2026-04-08 21:13:48 +0800

    Add mypy. Something got dragged along in test_core

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,10 +1,6 @@
 # Copyright 2023 ACCESS-NRI and contributors. See the top-level COPYRIGHT file for details.
 # SPDX-License-Identifier: Apache-2.0
 
-import pytest
-
-from intake_virtual_icechunk.core import IcechunkSource
-
 
 def test_placeholder():
     """Placeholder — replace with real tests."""
