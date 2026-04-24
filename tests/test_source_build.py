e4d454b25a1f19f72e23228e91cc507b50b225f9
Charles Turner <charles.turner@anu.edu.au>
2026-04-24 12:23:10 +0800

    Fix the whoopsies

--- tests/test_source_build.py
+++ tests/test_source_build.py
@@ -50,25 +50,20 @@ class TestIcechunkStoreBuilder:
         }
 
     @pytest.fixture
-    def om2_esmds_path(self, sample_data: Path) -> Path:
-        """
-        Fixture that provides the path to the intake-esm datastore for the OM2 model.
-        """
-        return sample_data / "access-om2" / "esmcat" / "access-om2.json"
-
-    @pytest.fixture
-    def om2_datastore(self, om2_esmds_path, intake_esm_kwargs) -> esm_datastore:
+    def om2_datastore(
+        self, local_om2_datastore_path, intake_esm_kwargs
+    ) -> esm_datastore:
         """
         Fixture that provides an intake-esm datastore for the OM2 model.
         """
 
         return intake.open_esm_datastore(
-            str(om2_esmds_path),
+            str(local_om2_datastore_path),
             **intake_esm_kwargs,
         )
 
     def test_init_infer_parser(
-        self, om2_esmds_path, om2_datastore, intake_esm_kwargs, tmpdir
+        self, local_om2_datastore_path, om2_datastore, intake_esm_kwargs, tmpdir
     ):
         """
         Initialisation without a parser should trigger parser inference, which
@@ -76,14 +71,14 @@ class TestIcechunkStoreBuilder:
         """
         dummy_store_path = tmpdir / "dummy_store.icechunk"
         builder = IcechunkStoreBuilder(
-            om2_esmds_path, intake_esm_kwargs, dummy_store_path
+            local_om2_datastore_path, intake_esm_kwargs, dummy_store_path
         )
 
         assert isinstance(builder.parser, virtualizarr.parsers.hdf.hdf.HDFParser)
         assert_frame_equal(builder.esm_ds.df, om2_datastore.df)
 
     def test_init_with_parser(
-        self, om2_esmds_path, om2_datastore, intake_esm_kwargs, tmpdir
+        self, local_om2_datastore_path, om2_datastore, intake_esm_kwargs, tmpdir
     ):
         """
         Initialisation with a parser should use the provided parser not instantiate
@@ -92,7 +87,7 @@ class TestIcechunkStoreBuilder:
         dummy_store_path = tmpdir / "dummy_store.icechunk"
         parser = virtualizarr.parsers.hdf.hdf.HDFParser
         builder = IcechunkStoreBuilder(
-            om2_esmds_path, intake_esm_kwargs, dummy_store_path, parser=parser
+            local_om2_datastore_path, intake_esm_kwargs, dummy_store_path, parser=parser
         )
 
         assert builder._esm_ds is None
@@ -111,7 +106,7 @@ class TestIcechunkStoreBuilder:
         ],
     )
     def test_infer_parser(
-        self, om2_esmds_path, intake_esm_kwargs, tmpdir, format_val, parser
+        self, local_om2_datastore_path, intake_esm_kwargs, tmpdir, format_val, parser
     ):
         """
         Mostly a regression test for now.
@@ -119,7 +114,7 @@ class TestIcechunkStoreBuilder:
 
         dummy_store_path = tmpdir / "dummy_store.icechunk"
         builder = IcechunkStoreBuilder(
-            om2_esmds_path, intake_esm_kwargs, dummy_store_path
+            local_om2_datastore_path, intake_esm_kwargs, dummy_store_path
         )
         from intake_esm.cat import DataFormat
 
